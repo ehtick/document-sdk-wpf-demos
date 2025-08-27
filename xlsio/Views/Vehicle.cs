9f4440afb62b8fa50c7aea957f889bfc215b04b5
Pandikumarsf2691 <pandikumar.duraivel@syncfusion.com>
2025-08-27 22:30:46 +0530

    976250-DocumentSDK

---
+++ xlsio/Views/Vehicle.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Xml.Serialization;
+
+namespace CollectionObjects
+{
+    #region Import Classes
+    public class Brand
+    {
+        private string m_brandName;
+        [DisplayNameAttribute("Brand")]
+        public string BrandName
+        {
+            get { return m_brandName; }
+            set { m_brandName = value; }
+        }
+
+        private VehicleType m_vehicleType;
+        public VehicleType VehicleType
+        {
+            get { return m_vehicleType; }
+            set { m_vehicleType = value; }
+        }
+
+        public Brand(string brandName)
+        {
+            m_brandName = brandName;
+        }
+        public Brand()
+        {
+
+        }
+    }
+
+    public class VehicleType
+    {
+        private string m_vehicleName;
+        [DisplayNameAttribute("Vehicle Type")]
+        public string VehicleName
+        {
+            get { return m_vehicleName; }
+            set { m_vehicleName = value; }
+        }
+
+        private Model m_model;
+        public Model Model
+        {
+            get { return m_model; }
+            set { m_model = value; }
+        }
+
+        public VehicleType(string vehicle)
+        {
+            m_vehicleName = vehicle;
+        }
+        public VehicleType()
+        {
+
+        }
+    }
+
+    public class Model
+    {
+        private string m_modelName;
+        [DisplayNameAttribute("Model")]
+        public string ModelName
+        {
+            get { return m_modelName; }
+            set { m_modelName = value; }
+        }
+
+        public Model(string name)
+        {
+            m_modelName = name;
+        }
+        public Model()
+        {
+
+        }
+    }
+    #endregion 
+}
+
+namespace ImportNestedCollection
+{
+    #region Import Classes
+
+    [XmlRootAttribute("BrandObjects")]
+    public class BrandObjects
+    {
+        [XmlElement("BrandObject")]
+        public BrandObject[] BrandObject { get; set; }
+    }
+    public class BrandObject
+    {
+        public string BrandName { get; set; }
+        public string VahicleType { get; set; }
+        public string ModelName { get; set; }
+    }
+
+    public class Brand
+    {
+        private string m_brandName;
+        [DisplayNameAttribute("Brand")]
+        public string BrandName
+        {
+            get { return m_brandName; }
+            set { m_brandName = value; }
+        }
+
+        private IList<VehicleType> m_vehicleTypes;
+        public IList<VehicleType> VehicleTypes
+        {
+            get { return m_vehicleTypes; }
+            set { m_vehicleTypes = value; }
+        }
+
+        public Brand(string brandName)
+        {
+            m_brandName = brandName;
+        }
+    }
+
+    public class VehicleType
+    {
+        private string m_vehicleName;
+        [DisplayNameAttribute("Vehicle Type")]
+        public string VehicleName
+        {
+            get { return m_vehicleName; }
+            set { m_vehicleName = value; }
+        }
+
+        private IList<Model> m_models;
+        public IList<Model> Models
+        {
+            get { return m_models; }
+            set { m_models = value; }
+        }
+
+        public VehicleType(string vehicle)
+        {
+            m_vehicleName = vehicle;
+        }
+    }
+
+    public class Model
+    {
+        private string m_modelName;
+        [DisplayNameAttribute("Model")]
+        public string ModelName
+        {
+            get { return m_modelName; }
+            set { m_modelName = value; }
+        }
+
+        public Model(string name)
+        {
+            m_modelName = name;
+        }
+    }
+    #endregion 
+}
+
